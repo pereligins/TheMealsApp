0ce2f7ef6a88cf3902fd92c2b1d36634d6310d4a
alexey <pereligins@gmail.com>
2019-11-06 02:07:52 +0300

    Favorite icon

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import {HeaderButtons, Item} from "react-navigation-header-buttons";
 import {Button, StyleSheet, Text, View} from 'react-native';
+import {MEALS} from "../data/mock-data";
+import CustomHeaderButton  from "../components/HeaderButton";
 
 const MealDetailScreen = props => {
+
+    const mealId = props.navigation.getParam('mealId');
+    const selectedMeal = MEALS.find(meal => meal.id === mealId)
+
     return (<View style={styles.screen}>
-        <Text>MealDetailScreen</Text>
+        <Text>{selectedMeal.title}</Text>
         <Button title='Go to The Categories' onPress={() => {props.navigation.popToTop()}}/>
     </View>);
+};
+
+MealDetailScreen.navigationOptions = navigationData =>  {
+    const mealId = navigationData.navigation.getParam('mealId');
+    const selectedMeal = MEALS.find(meal => meal.id === mealId);
+
+    return {
+        headerTitle: selectedMeal.title,
+        headerRight: <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+            <Item title='Favorite' iconName='ios-star' onPress={() => {console.log('FAV')}}/>
+        </HeaderButtons>
+    }
+
 };
 
 const styles = StyleSheet.create({
